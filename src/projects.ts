3a730fc3c10415076574927c1a83c7d5b657ac59
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-04-22 18:10:51 +0200

    feat: created interface

---
+++ src/projects.ts
@@ -0,0 +1,16 @@
+interface IProject {
+    id: number;
+    title: string;
+    img?: {
+        src: string;
+        alt: string;
+        width: number;
+        height: number;
+    };
+    description: string;
+    tech: string[];
+    demo: string;
+    github: string;
+    created_at: string;
+}
+
