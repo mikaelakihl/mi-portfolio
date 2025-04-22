184ae1728ff88112ce265a99ede9d9abae4f7932
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-04-22 18:12:29 +0200

    feat: print out project with template literal

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
+import project from "./projects";
+
+const projects = document.querySelector('#projectContainer');
+
+if (projects) {
+    projects.innerHTML = project
+    .map(p => `
+      <h3>${p.title}</h3>
+      <img src="${p.img?.src}" width="${p.img?.width}" height="${p.img?.height}">
+      <p>${p.description}</p>
+        <span>${p.tech}</span>
+        <a href="${p.demo}">View Demo</a>
+        <a href="${p.github}">Go to Github</a>
+      `)
+      .join('');
+
+
+
+
+}
 
